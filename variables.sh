616beda3080baf0de21ae104ef3d8c06ec7c6deb
RJ <rjseven007@gmail.com>
2019-11-08 21:26:16 +0000

    Changes

---
+++ variables.sh
@@ -0,0 +1,9 @@
+# RJ Setup
+RjSetup="$HOME/RJ-Setup"
+RjBackup="$RjSetup/Backup"
+
+
+# Git Repos
+GitRjSetup="git@github.com:RJHimself/RJ-Setup-Desktop.git"
+GitRjBackup="git@github.com:RJHimself/RJ-Setup-Desktop-Backup.git"
+GitBrightBash="git@github.com:RJHimself/Bright-Bash.git"
